5bf7a62771c097bcf47eefbe4d17115f60742cb8
Frost Ming <me@frostming.com>
2026-03-07 20:08:29 +0800

    Remove Astro base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dagebot.github.io',
-  base: '/daily-notes',
   integrations: [react()]
 });
